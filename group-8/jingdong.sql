1f5ff1c9faae56d607d7a1e8ab7270cd9f622b33
wmw <1842982500@qq.com>
2021-04-27 21:06:58 +0800

    book's sql

---
+++ group-8/jingdong.sql
@@ -0,0 +1,10 @@
+INSERT INTO `jingdong` VALUES (1, 'Python', 165.50, '人民邮电出版社');
+INSERT INTO `jingdong` VALUES (2, '三剑客：Python', 87.30, '人民邮电出版社');
+INSERT INTO `jingdong` VALUES (3, '从入门到实践第2版+快速上手第2版+极客', 37.50, '明日科技京东自营旗舰店');
+INSERT INTO `jingdong` VALUES (4, '（套装共3册）', 86.60, '电子工业出版社');
+INSERT INTO `jingdong` VALUES (5, 'JavaScript高级程序设计 第4版(图灵出品）', 59.00, '人民邮电出版社');
+INSERT INTO `jingdong` VALUES (6, '零基础学Python', 49.50, '电子工业出版社');
+INSERT INTO `jingdong` VALUES (7, '入门 项目实践(同步视频）（Python3全彩版）', 79.30, '机械工业出版社自营官方旗舰店');
+INSERT INTO `jingdong` VALUES (8, 'C++ Primer（中文版 第5版）(博文视点出品)', 72.00, '机械工业出版社自营官方旗舰店');
+INSERT INTO `jingdong` VALUES (9, 'C++ Primer Plus 第6版 中文版(异步图书出品)', 54.00, '人民邮电出版社');
+INSERT INTO `jingdong` VALUES (10, '【包邮】labuladong的算法小抄(Python Java C++零基础)(博文视点出品)', 82.90, '人民邮电出版社');
